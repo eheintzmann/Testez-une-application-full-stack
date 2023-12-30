5b19bd766093876bb20d03097f1d3ff735f7a7f7
Eric Heintzmann <heintzmann.eric@free.fr>
2023-12-30 16:59:11 +0100

    Update LoginComponent Integration test suite

--- front/src/app/features/auth/components/login/login.component.int.test.ts
+++ front/src/app/features/auth/components/login/login.component.int.test.ts
@@ -1,5 +1,5 @@
 import { LoginComponent } from './login.component';
-import { ComponentFixture, fakeAsync, flush, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SessionService } from '../../../../services/session.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,77 +10,152 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { expect } from '@jest/globals';
-
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
+import { Observable, of } from 'rxjs';
+import { SessionInformation } from '../../../../interfaces/sessionInformation.interface';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 describe('LoginComponent Integration test suite', () => {
+  let loader: HarnessLoader;
   let fixture: ComponentFixture<LoginComponent>;
-  let loginElement: HTMLElement;
-  let emailElement: HTMLInputElement | null;
-  let passwordElement: HTMLInputElement | null;
-  let submitButton: HTMLButtonElement | null;
 
-  const setValues = (email: string, password: string) => {
-    emailElement!.setRangeText(email);
-    passwordElement!.setRangeText(password);
+  let emailInput: MatInputHarness;
+  let passwordInput: MatInputHarness;
+  let submitButton: MatButtonHarness;
 
-    emailElement!.dispatchEvent(new Event('input'));
-    passwordElement!.dispatchEvent(new Event('input'));
 
-    fixture.detectChanges();
-    flush();
-  }
+  describe('when user is filling login form', () => {
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      providers: [SessionService],
-      imports: [
-        RouterTestingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        MatCardModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        ReactiveFormsModule]
-    }).compileComponents();
+    const setValues = async (email: string, password: string) => {
+      await emailInput.setValue(email);
+      await passwordInput.setValue(password);
 
-    fixture = TestBed.createComponent(LoginComponent);
-    fixture.detectChanges();
+      return
+    }
 
-    loginElement = fixture.nativeElement;
-    emailElement = loginElement.querySelector('input[formControlName="email"]');
-    passwordElement = loginElement.querySelector('input[formControlName="password"]');
-    submitButton = loginElement.querySelector('button[type="submit"]');
-  });
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [LoginComponent],
+        providers: [SessionService],
+        imports: [
+          RouterTestingModule,
+          BrowserAnimationsModule,
+          HttpClientModule,
+          MatCardModule,
+          MatIconModule,
+          MatFormFieldModule,
+          MatInputModule,
+          ReactiveFormsModule]
+      }).compileComponents();
 
-  it('should hide #Submit if #Email is empty', fakeAsync(() => {
-    setValues('', 'password')
+      fixture = TestBed.createComponent(LoginComponent);
+      fixture.detectChanges();
+      loader = TestbedHarnessEnvironment.loader(fixture);
 
-    expect(submitButton!.disabled).toBeTruthy();
-  }));
+      emailInput = await loader.getHarness<MatInputHarness>(MatInputHarness.with({selector: 'input[formControlName="email"]'}));
+      passwordInput = await loader.getHarness<MatInputHarness>(MatInputHarness.with({selector: 'input[formControlName="password"]'}));
+      submitButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({selector: 'button[type="submit"]'}));
+    });
 
-  it('should hide #Submit if #Email is invalid',fakeAsync(() => {
-    setValues('invalid', 'password')
+    it('should disable #Submit button if #Email is empty', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
 
-    expect(submitButton!.disabled).toBeTruthy();
-  }));
+      await setValues('', 'password')
+      expect(await submitButton.isDisabled()).toBeTruthy();
+    });
 
-  it('should hide #Submit if #Password is empty',fakeAsync(() => {
-    setValues('test@test.com', '');
-    expect(submitButton!.disabled).toBeTruthy();
-  }));
+    it('should disable #Submit button if #Email is invalid', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
 
-  it('should hide #Submit if #Password is too short', fakeAsync(() => {
-    setValues('test@test.com', '1');
+      await setValues('invalid', 'password')
+      expect(await submitButton.isDisabled()).toBeTruthy();
+    });
 
-    expect(submitButton!.disabled).toBeTruthy();
-  }));
+    it('should disable #Submit button if #Password is empty', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
 
-  it('should not hide #Submit', fakeAsync(() => {
-    setValues('test@test.com', 'password')
+      await setValues('test@test.com', '');
+      expect(await submitButton.isDisabled()).toBeTruthy();
+    });
 
-    expect(submitButton!.disabled).toBeFalsy();
-  }));
+    it('should disable #Submit button if #Password is too short', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
+
+      await setValues('test@test.com', '1');
+      expect(await submitButton.isDisabled()).toBeTruthy();
+    });
+
+    it('should enable #Submit button', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
+
+      await setValues('test@test.com', 'password')
+      expect(await submitButton.isDisabled()).toBeFalsy();
+    });
+  })
+
+
+  describe('when user is connecting', () => {
+    let spyRouter: jest.SpyInstance<Promise<boolean>>;
+
+    const mockAuthService = {
+      login(loginRequest: LoginRequest): Observable<SessionInformation> {
+        return of<SessionInformation>(
+          {
+            token: 'token',
+            type: 'type',
+            id: 0,
+            username: loginRequest.email,
+            firstName: 'FirstName',
+            lastName: 'LastName',
+            admin: false
+          }
+        )
+      }
+    }
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [LoginComponent],
+        providers: [
+          { provide: AuthService, useValue: mockAuthService },
+          SessionService
+        ],
+        imports: [
+          RouterTestingModule,
+          BrowserAnimationsModule,
+          HttpClientModule,
+          MatCardModule,
+          MatIconModule,
+          MatFormFieldModule,
+          MatInputModule,
+          ReactiveFormsModule]
+      }).compileComponents();
+
+      const router: Router = TestBed.inject(Router);
+      spyRouter = jest.spyOn(router, 'navigate');
+
+      fixture = TestBed.createComponent(LoginComponent);
+      fixture.detectChanges();
+      loader = TestbedHarnessEnvironment.loader(fixture);
+
+      emailInput = await loader.getHarness<MatInputHarness>(MatInputHarness.with({selector: 'input[formControlName="email"]'}));
+      passwordInput = await loader.getHarness<MatInputHarness>(MatInputHarness.with({selector: 'input[formControlName="password"]'}));
+      submitButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({selector: 'button[type="submit"]'}));
+    });
+    it('should connect user', async () => {
+      expect(await submitButton.isDisabled()).toBeTruthy();
+
+      await emailInput.setValue('test@test.com');
+      await passwordInput.setValue('password');
+      expect(await submitButton.isDisabled()).toBeFalsy();
+
+      await submitButton.click();
+      expect(spyRouter).toHaveBeenCalledWith(['/sessions'])
+    });
+  })
 });
